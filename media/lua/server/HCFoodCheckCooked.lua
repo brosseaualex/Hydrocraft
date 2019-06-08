65c3dacf6208a7c927d914d8c4fc7965786a8cf2
Alexandre Brosseau <brosseaualex@gmail.com>
2019-06-07 23:32:27 -0400

    Seperated CheckIfCooked functions into seperate file

---
+++ media/lua/server/HCFoodCheckCooked.lua
@@ -0,0 +1,141 @@
+-- ***********************************************************
+-- **                    DemolitionDerby                    **
+-- **			   Food CheckIfCooked Functions				**
+-- ***********************************************************
+
+--Food Cookies OnTest
+
+function RemoveCookies_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCHomemadecookietray" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveChocolateChipCookies_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCChipcookietray" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveWhiteChocolateCookies_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCCookiewhitetray" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemovePeanutButterCookies_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCCookiepeanutbuttertray" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveChocolateCookies_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCChocolatecookietray" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveDoubleChocolateCookies_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCDoublechocolatecookietray" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveGingerbreadManCookies_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCGingerbreadmantray" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveChristmasCookies_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCChristmascookietray" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+-- Food Jello OnTest
+function RemoveAppleJello_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCJelloappleprep" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveBlueberryJello_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCJelloblueberryprep" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveCherryJello_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCJellocherryprep" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveLemonJello_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCJellolemonprep" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveOrangeJello_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCJelloorangeprep" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveLimeJello_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCJellolimeprep" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+function RemoveWatermelonJello_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCJellowatermelonprep" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function RemoveStrawberryJello_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCJellostrawberryprep" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+--Food Pizza OnTest
+
+function CutBoringPizza_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCBoringpizza" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function CutHCSimplePizza_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCSimplepizza" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
+
+function CutTastyPizza_CheckIfCooked(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCTastypizza" then
+        return sourceItem:isCooked()
+    end
+    return true
+end
