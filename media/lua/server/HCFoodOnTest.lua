8d6bf74e9df02e684637946b4b9acaf5659dd644
Alexandre Brosseau <brosseaualex@gmail.com>
2019-08-09 14:21:33 -0400

    Combined Food/Juices OnTest in same file - Renamed food art

--- media/lua/server/HCFoodOnTest.lua
+++ media/lua/server/HCFoodOnTest.lua
@@ -3,6 +3,8 @@
 -- **			      Food OnTest Functions					**
 -- ***********************************************************
 
+-- ****** FOOD ONTEST START ******
+
 --Food Cookies OnTest
 
 function RemoveCookies_CheckIfCooked(sourceItem, result)
@@ -537,3 +539,149 @@ function JarLimeCurd_CheckIfCooked(sourceItem, result)
     end
     return true
 end
+
+-- ****** FOOD ONTEST END ******
+
+-- ****** JUICES ONTEST START ******
+
+function MakeLimeJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.HCLime" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeLemonJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Hydrocraft.Lemon" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeAppleJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Apple" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeCarrotJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Carrots" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeGrapeJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Grapes" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeOrangeJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Orange" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeStrawberryJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Strewberrie" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeTomatoJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Tomato" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeWatermelonFullJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Watermelon" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeWatermelonSliceJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "WatermelonSliced" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeWatermelonSmashedJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "WatermelonSmashed" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakePineappleJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Pineapple" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeBerryBlueJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "BerryBlue" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeBerryBlackJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "BerryBlack" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeBerryGeneric1Juice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "BerryGeneric1" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeBerryGeneric2Juice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "BerryGeneric2" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeBerryGeneric3Juice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "BerryGeneric3" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeBerryGeneric4Juice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "BerryGeneric4" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeBerryGeneric5Juice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "BerryGeneric5" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+function MakeCherryJuice_CheckIfFresh(sourceItem, result)
+    if sourceItem:getFullType() == "Cherry" then
+        return sourceItem:isFresh()
+    end
+    return true
+end
+
+-- ****** JUICES ONTEST END ******
